027d5cf501897774b53fd75d2435ddcd93645ecd
Elijah Pina <pina.elijah@gmail.com>
2026-04-28 08:44:24 -0700

    I finished Practice01

--- genericPractice.java
+++ genericPractice.java
@@ -1,30 +1,38 @@
-
 import java.util.*;
 
 public class genericPractice {
     public static void main(String[] args) {
+        List<Integer> ints = Arrays.asList(1,2,3);
+        List<Double> doubles = Arrays.asList(1.5,2.5);
 
         List<Number> numbers = new ArrayList<>();
         List<Object> objects = new ArrayList<>();
 
-        // This is a test to push to github
-
+        printNumbers(ints);
+        printNumbers(doubles);
         addInts(numbers);
         addInts(objects);
-        
 
 
+    }
 
+    public static void printNumbers(List<? extends Number> list) {
+        // Loop and print each number
+        for (Number n : list) {
+            System.out.println("Here is your number: " + n);
+        }
     }
     
     public static void addInts(List<? super Integer> list) {
-        
-            list.add(10);
-            list.add(20);
-        
-        
+        // add 10 and 20
+        list.add(10);
+        list.add(20);
+        // Loop and print each element
         for (Object n : list) {
-            System.out.println("Item: " + n);
+            System.out.println(n);
         }
+
     }
+  
+   
 }
